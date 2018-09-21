50cd3984a80fb4429efbaf5fa319286585733a1c
Patrick Canny <patrickcanny@ku.edu>
2018-09-21 12:29:41 -0500

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='kaggle shakespeare competition/ project for 731',
+    author='patrick canny',
+    license='MIT',
+)
